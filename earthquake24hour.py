e288cc698e6e60303466ea99fc9edb1c55ae2b06
Jamesl65 <67878127+Jamesl65@users.noreply.github.com>
2025-02-07 22:59:12 -0500

    Add files via upload
    Clear up

--- earthquake24hour.py
+++ earthquake24hour.py
@@ -1,5 +1,5 @@
 # 
-# Example file for parsing and processing JSON
+# Earth quake last 24 hours
 #
 
 import urllib.request 
@@ -9,43 +9,25 @@ import datetime
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
-    
-    # now we can access the contents of the JSON like any other Python object
-    #if "title" in theJSON["metadata"]:
+ 
     print(theJSON["metadata"]["title"])
-
     count  = theJSON["metadata"]["count"]
     print(str(count) + " events recorded")
 
     print("Richter scale | time    | Place ")
     for i in theJSON["features"]:
-
         magnitude = i["properties"]["mag"] + 0.00 
         epoch = i["properties"]["time"]
         epoch_date_time = datetime.datetime.fromtimestamp(epoch/1000)  
         print('{0:.2f}'.format(magnitude), epoch_date_time, i["properties"]["place"])
-    # output the number of events, plus the magnitude and each event name  
-
-    
-    # for each event, print the place where it occurred
-
-
-    # print the events that only have a magnitude greater than 4
-
-
-    # print only the events where at least 1 person reported feeling something
-
-  
+ 
 def main():
-    # define a variable to hold the source URL
     # In this case we'll use the free data feed from the USGS
     # This feed lists all earthquakes for the last day larger than Mag 2.5
     urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
 
-    # Open the URL and read the data
     webUrl = urllib.request.urlopen(urlData)
     # print ("result code: " + str(webUrl.getcode()))
-  
     data = webUrl.read()
     printResults(data)
 
